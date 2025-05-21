d36cc32518af18aefb36fc5a100f136890978075
ghaniymadea <agaghaniymadea@gmail.com>
2025-05-21 13:54:55 +0700

    Add files via upload

---
+++ Latihan1.java
@@ -0,0 +1,13 @@
+
+public class Latihan1 {
+  public static void main(String[] args) {
+    String nama = "ghaniy";
+    String kelas = "X PPLG 2";
+    double B_badan = 58.00;
+System.out.println("Nama Saya : "+nama);
+System.out.println("kelas : " +kelas);
+System.out.print("Berat badan :");
+System.out.println(B_badan);
+
+  }  
+}
